ffeffeddc18ed37d87bcc41198b4a90283a34bcd
Nicolepinto09 <nicole.pinto@correo.unimet.edu.ve>
2024-05-26 15:12:14 -0400

    teclado

--- main.js
+++ main.js
@@ -3,11 +3,16 @@ const BotonStart = document.getElementById("start");
 const LetrasUsadas = document.getElementById("LetrasUsadas");
 
 
-function obtenerPalabraRandom(palabras) {
-    const indiceRandom = Math.floor(Math.random() * palabras.length);
-    const palabraRandom = palabras[indiceRandom];
-    console.log(palabraRandom, pista);
+const tecladoDiv = document.querySelector(".teclado");
+
+
+for (let letra = 97; letra <= 122; letra++) {
+    const boton = document.createElement("button");
+    boton.innerText = String.fromCharCode(letra);
+    tecladoDiv.appendChild(boton)
+
 }
+
 
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
