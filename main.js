898d5b74aa1aa2761d7f01fc93f5f947ab58f40e
Nicolepinto09 <nicole.pinto@correo.unimet.edu.ve>
2024-05-26 13:09:02 -0400

    modificacion de seleccionar palabra random

--- main.js
+++ main.js
@@ -2,11 +2,17 @@ const palabra = document.getElementById("palabra");
 const BotonStart = document.getElementById("start");
 const LetrasUsadas = document.getElementById("LetrasUsadas");
 
+
+function obtenerPalabraRandom(palabras) {
+    const indiceRandom = Math.floor(Math.random() * palabras.length);
+    const palabraRandom = palabras[indiceRandom];
+    console.log(palabraRandom, pista);
+}
+
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 ctx.canvas.width = 0;
 ctx.canvas.height = 0;
-
 
 
 let palabraSelec;
